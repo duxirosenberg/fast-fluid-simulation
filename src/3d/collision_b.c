84dbbea8a293d5496a495bb8ed79120b0122f9df
S.Klapproth <severikl@student.ethz.ch>
2024-06-09 19:50:39 +0200

    collision adaptive to all grid sizes

--- src/3d/collision_b.c
+++ src/3d/collision_b.c
@@ -68,6 +68,82 @@ void collision_arrays(int nX, int nY, int nZ, int direction_size, double tau, do
 
 
 
+void collision_3_old(struct LBMarrays* S) {
+    const double cs2 = S->c_s*S->c_s;
+    const double cs4 = cs2*cs2;
+    const double tau_inv =  1.0/S->tau;
+    const double omtauinv = 1.0 - tau_inv;
+    const double cs2_inv =  1.0 / cs2;
+    const double half_cs2_inv = 0.5 * cs2_inv;;
+    const double half_cs4_inv = 0.5 / cs4;
+        
+    for(int i = 0; i < S->direction_size; i++) {
+        //  switched is half as fast for 8 and 32 ..
+        const double weight = S->weights[i];
+        const int directionX = S->directions[3 * i];
+        const int directionY = S->directions[3 * i + 1];
+        const int directionZ = S->directions[3 * i + 2];
+
+        for(int feqIndex = 0; feqIndex < S->nXYZ; ++feqIndex) {
+
+            const int index = feqIndex + i * S->nXYZ;
+            const double velocityX = S->velocity_field[3 * feqIndex];
+            const double velocityY = S->velocity_field[3 * feqIndex + 1];
+            const double velocityZ = S->velocity_field[3 * feqIndex + 2];
+
+            const double norm_square = velocityX * velocityX + velocityY * velocityY + velocityZ * velocityZ;
+            const double dot_product = velocityX * directionX + velocityY * directionY + velocityZ * directionZ;
+
+                    const double feq = 
+                    weight * S->density_field[feqIndex] * (1.0 + dot_product* cs2_inv
+                    + dot_product * dot_product * half_cs4_inv - norm_square * half_cs2_inv);
+
+            S->previous_particle_distributions[index] = omtauinv * S->particle_distributions[index] + tau_inv * feq;
+        }
+    }
+}
+
+
+
+void collision_6_old(struct LBMarrays* S) {
+    // const int iBB = 9;
+    const int nBB = BLOCKSIZE_COL > S->nXYZ ? S->nXYZ : BLOCKSIZE_COL;
+
+    const double cs2 = S->c_s*S->c_s;
+    const double cs4 = cs2*cs2;
+    const double tau_inv =  1.0/S->tau;
+    const double omtauinv = 1.0 - tau_inv;
+    const double cs2_inv =  1.0 / cs2;
+    const double half_cs2_inv = 0.5 * cs2_inv;;
+    const double half_cs4_inv = 0.5 / cs4;
+
+    for(int nxyz = 0; nxyz < S->nXYZ; nxyz+=nBB) {
+        for(int i = 0; i < S->direction_size; i++) {
+            const double weight = S->weights[i];
+            const int directionX = S->directions[3 * i];
+            const int directionY = S->directions[3 * i + 1];
+            const int directionZ = S->directions[3 * i + 2];
+
+            for(int feqIndex = nxyz; feqIndex < nxyz+nBB; ++feqIndex) {
+                const double velocityX = S->velocity_field[3 * feqIndex];
+                const double velocityY = S->velocity_field[3 * feqIndex + 1];
+                const double velocityZ = S->velocity_field[3 * feqIndex + 2];
+
+
+                const double dot_product = velocityX * directionX + velocityY * directionY + velocityZ * directionZ;
+                const double norm_square = velocityX * velocityX + velocityY * velocityY + velocityZ * velocityZ;
+
+                    const double feq = 
+                    weight * S->density_field[feqIndex] * (1.0 + dot_product* cs2_inv
+                    + dot_product * dot_product * half_cs4_inv - norm_square * half_cs2_inv);
+
+                    int index = feqIndex + i * S->nXYZ;
+                    S->previous_particle_distributions[index] = omtauinv * S->particle_distributions[index] + tau_inv * feq;
+                
+            }
+        }
+    }
+}
 
 void collision_SSA(struct LBMarrays* S) {
 
@@ -204,6 +280,60 @@ void collision_SSA_u(struct LBMarrays* S) {
 }
 
 
+void collision_SSA2(struct LBMarrays* S) {
+    const int nBB = BLOCKSIZE_COL > S->nXYZ ? S->nXYZ : BLOCKSIZE_COL;
+    const double cs2 = S->c_s*S->c_s;
+    const double cs4 = cs2*cs2;
+    const double tau_inv =  1.0/S->tau;
+    const double omtauinv = 1.0 - tau_inv;
+    const double cs2_inv =  1.0 / cs2;
+    const double half_cs2_inv = 0.5 * cs2_inv;
+    const double half_cs4_inv = 0.5 / cs4;
+
+
+    for(int nxyz = 0; nxyz < S->nXYZ; nxyz+=nBB) {
+        for(int i = 0; i < S->direction_size; i++) {
+            const double weight = S->weights[i];
+            const int directionX = S->directions[3 * i];
+            const int directionY = S->directions[3 * i + 1];
+            const int directionZ = S->directions[3 * i + 2];
+
+            for(int feqIndex = nxyz; feqIndex < nxyz+nBB; ++feqIndex) {
+                const int index = feqIndex + i * S->nXYZ;
+
+                const double velocityX = S->velocity_fieldX[feqIndex];
+                const double velocityY = S->velocity_fieldY[feqIndex];
+                const double velocityZ = S->velocity_fieldZ[feqIndex];
+
+                const double vdx = velocityX * directionX;
+                const double vdy = velocityY * directionY;
+                const double vdz = velocityZ * directionZ;
+                const double vdxy         = vdx + vdy ;
+                const double dot_product  = vdxy + vdz;
+
+                const double vvx = velocityX * velocityX;
+                const double vvy = velocityY * velocityY;
+                const double vvz = velocityZ * velocityZ;
+                const double vvxy         = vvx + vvy;
+                const double norm_square  = vvxy + vvz;
+
+
+                const double A      = weight * S->density_field[feqIndex];
+                const double B      = dot_product * dot_product;
+                const double C      = norm_square * half_cs2_inv;
+                const double D      = dot_product*cs2_inv;
+                const double E      = B * half_cs4_inv;
+                const double F      = 1.0 + D;
+                const double G      = F +   E;
+                const double H      = G -   C;
+                const double feq    = A *   H;
+                const double I      = tau_inv * feq;
+                const double J      = omtauinv*S->particle_distributions[index];
+                S->previous_particle_distributions[index] = I+J;
+            }
+        }
+    }
+}
 
 void collision_SSA2_u(struct LBMarrays* S) {
 
@@ -1105,6 +1235,265 @@ void collision_AVX3_u(struct LBMarrays* S) {
 }
 
 
+
+
+
+
+void collision_AVX4_u(struct LBMarrays* S) {
+
+    // const int nBB = 1024;512
+    const int nBB = BLOCKSIZE_COL > S->nXYZ ? S->nXYZ : BLOCKSIZE_COL;
+    const double tau_inv = 1/S->tau;
+    const double cs2 = S->c_s*S->c_s;
+    const double cs4 = cs2*cs2;
+
+
+        __m256d ones_pd      = _mm256_set1_pd(1.0);
+        __m256d CS2inv_pd    = _mm256_set1_pd(1.0/cs2);
+        __m256d twoCS2inv_pd = _mm256_set1_pd(0.5/cs2);
+        __m256d twoCS4inv_pd = _mm256_set1_pd(0.5/cs4);
+        __m256d TAUinv_pd    = _mm256_set1_pd(tau_inv);
+        __m256d omTAUinv_pd  = _mm256_set1_pd(1.0 - tau_inv);
+       
+
+        for(int nxyz = 0; nxyz < S->nXYZ; nxyz+=nBB) {
+            for(int i = 0; i < S->direction_size; i++) { 
+
+                    __m256d weight_4 = _mm256_set1_pd(S->weights[i]);
+                    __m256d tmpA     = _mm256_mul_pd(weight_4,TAUinv_pd);
+
+                    __m256d dirX = _mm256_set1_pd(S->directions[3 * i]);
+                    __m256d dirY = _mm256_set1_pd(S->directions[3 * i+1]);
+                    __m256d dirZ = _mm256_set1_pd(S->directions[3 * i+2]);
+
+                for(int feqIndex = nxyz; feqIndex < nxyz+nBB; feqIndex+=8) {
+
+                    
+                    const int b_feqIndex = feqIndex + 4;
+                    const int index      = feqIndex   + i * S->nXYZ;
+                    const int b_index    = b_feqIndex + i * S->nXYZ;                    
+                    
+                    
+
+                    __m256d velX = _mm256_loadu_pd(S->velocity_fieldX + feqIndex);
+                    __m256d velY = _mm256_loadu_pd(S->velocity_fieldY + feqIndex);
+                    __m256d velZ = _mm256_loadu_pd(S->velocity_fieldZ + feqIndex);
+
+                    __m256d b_velX = _mm256_loadu_pd(S->velocity_fieldX + b_feqIndex);
+                    __m256d b_velY = _mm256_loadu_pd(S->velocity_fieldY + b_feqIndex);
+                    __m256d b_velZ = _mm256_loadu_pd(S->velocity_fieldZ + b_feqIndex);
+
+
+                                                                                     
+
+
+                    __m256d df_pd = _mm256_load_pd(S->density_field          + feqIndex);
+                    __m256d PD_pd = _mm256_load_pd(S->particle_distributions + index);
+
+                    __m256d b_df_pd = _mm256_load_pd(S->density_field          + b_feqIndex);
+                    __m256d b_PD_pd = _mm256_load_pd(S->particle_distributions + b_index);
+
+                    
+
+                    __m256d dotX     = _mm256_mul_pd(   velX, dirX);
+                    __m256d dotXY    = _mm256_fmadd_pd( velY, dirY, dotX);
+                    __m256d dv_dot    = _mm256_fmadd_pd(velZ, dirZ, dotXY);
+                    __m256d normX     = _mm256_mul_pd(  velX, velX);
+                    __m256d normXY    = _mm256_fmadd_pd(velY, velY, normX);
+                    __m256d v_norm    = _mm256_fmadd_pd(velZ, velZ, normXY);
+
+
+                    __m256d b_dotX     = _mm256_mul_pd(   b_velX, dirX);
+                    __m256d b_dotXY    = _mm256_fmadd_pd( b_velY, dirY,   b_dotX);
+                    __m256d b_dv_dot    = _mm256_fmadd_pd(b_velZ, dirZ,   b_dotXY);
+                    __m256d b_normX     = _mm256_mul_pd(  b_velX, b_velX);
+                    __m256d b_normXY    = _mm256_fmadd_pd(b_velY, b_velY, b_normX);
+                    __m256d b_v_norm    = _mm256_fmadd_pd(b_velZ, b_velZ, b_normXY);
+
+
+                
+                    // ///////////////////////////
+                    
+
+                    __m256d tmpB          = _mm256_mul_pd(    df_pd,       tmpA);
+                    __m256d tmpC          = _mm256_mul_pd(    omTAUinv_pd, PD_pd);
+                    __m256d A              = _mm256_fmadd_pd( dv_dot,      twoCS2inv_pd, ones_pd);
+                    __m256d B              = _mm256_fmsub_pd( dv_dot,      A,            v_norm);
+                    __m256d C              = _mm256_fmadd_pd( B,           twoCS2inv_pd, ones_pd);
+                    __m256d D              = _mm256_fmadd_pd( tmpB,        C,            tmpC);
+
+
+                    __m256d b_tmpB          = _mm256_mul_pd(    b_df_pd,       tmpA);
+                    __m256d b_tmpC          = _mm256_mul_pd(    omTAUinv_pd,   b_PD_pd);
+                    __m256d b_A              = _mm256_fmadd_pd( b_dv_dot,      twoCS2inv_pd,   ones_pd);
+                    __m256d b_B              = _mm256_fmsub_pd( b_dv_dot,      b_A,            b_v_norm);
+                    __m256d b_C              = _mm256_fmadd_pd( b_B,           twoCS2inv_pd,   ones_pd);
+                    __m256d b_D              = _mm256_fmadd_pd( b_tmpB,        b_C,            b_tmpC);
+
+                    
+                    _mm256_storeu_pd(S->previous_particle_distributions+index, D);
+                    _mm256_storeu_pd(S->previous_particle_distributions+b_index, b_D);
+            }
+        }
+    }
+}
+
+
+
+void collision_AVX4_u2(struct LBMarrays* S) {
+
+    const int nBB = BLOCKSIZE_COL > S->nXYZ ? S->nXYZ : BLOCKSIZE_COL;
+    const double tau_inv = 1/S->tau;
+    const double cs2 = S->c_s*S->c_s;
+    const double cs4 = cs2*cs2;
+
+
+        __m256d ones_pd      = _mm256_set1_pd(1.0);
+        __m256d CS2inv_pd    = _mm256_set1_pd(1.0/cs2);
+        __m256d twoCS2inv_pd = _mm256_set1_pd(0.5/cs2);
+        __m256d twoCS4inv_pd = _mm256_set1_pd(0.5/cs4);
+        __m256d TAUinv_pd    = _mm256_set1_pd(tau_inv);
+        __m256d omTAUinv_pd  = _mm256_set1_pd(1.0 - tau_inv);
+        
+
+        for(int nxyz = 0; nxyz < S->nXYZ; nxyz+=nBB) {
+            for(int i = 0; i < S->direction_size; i++) { 
+
+                    __m256d weight_4 = _mm256_set1_pd(S->weights[i]);
+                    __m256d tmpA     = _mm256_mul_pd(weight_4,TAUinv_pd);
+
+                    __m256d dirX = _mm256_set1_pd(S->directions[3 * i]);
+                    __m256d dirY = _mm256_set1_pd(S->directions[3 * i+1]);
+                    __m256d dirZ = _mm256_set1_pd(S->directions[3 * i+2]);
+
+                for(int feqIndex = nxyz; feqIndex < nxyz+nBB; feqIndex+=16) {
+
+                    
+                    const int b_feqIndex = feqIndex + 4;
+                    const int c_feqIndex = feqIndex + 8;
+                    const int d_feqIndex = feqIndex + 12;
+                    const int index      = feqIndex   + i * S->nXYZ;
+                    const int b_index    = b_feqIndex + i * S->nXYZ;    
+                    const int c_index    = c_feqIndex + i * S->nXYZ;    
+                    const int d_index    = d_feqIndex + i * S->nXYZ;      
+
+                    // LOADING      
+                    
+                    __m256d velX = _mm256_loadu_pd(S->velocity_fieldX + feqIndex);
+                    __m256d velY = _mm256_loadu_pd(S->velocity_fieldY + feqIndex);
+                    __m256d velZ = _mm256_loadu_pd(S->velocity_fieldZ + feqIndex);
+
+                    __m256d b_velX = _mm256_loadu_pd(S->velocity_fieldX + b_feqIndex);
+                    __m256d b_velY = _mm256_loadu_pd(S->velocity_fieldY + b_feqIndex);
+                    __m256d b_velZ = _mm256_loadu_pd(S->velocity_fieldZ + b_feqIndex);
+
+                    __m256d c_velX = _mm256_loadu_pd(S->velocity_fieldX + c_feqIndex);
+                    __m256d c_velY = _mm256_loadu_pd(S->velocity_fieldY + c_feqIndex);
+                    __m256d c_velZ = _mm256_loadu_pd(S->velocity_fieldZ + c_feqIndex);
+
+                    __m256d d_velX = _mm256_loadu_pd(S->velocity_fieldX + d_feqIndex);
+                    __m256d d_velY = _mm256_loadu_pd(S->velocity_fieldY + d_feqIndex);
+                    __m256d d_velZ = _mm256_loadu_pd(S->velocity_fieldZ + d_feqIndex);
+
+
+
+
+                    __m256d df_pd = _mm256_load_pd(S->density_field          + feqIndex);
+                    __m256d PD_pd = _mm256_load_pd(S->particle_distributions + index);
+
+                    __m256d b_df_pd = _mm256_load_pd(S->density_field          + b_feqIndex);
+                    __m256d b_PD_pd = _mm256_load_pd(S->particle_distributions + b_index);
+
+                    __m256d c_df_pd = _mm256_load_pd(S->density_field          + c_feqIndex);
+                    __m256d c_PD_pd = _mm256_load_pd(S->particle_distributions + c_index);
+
+                    __m256d d_df_pd = _mm256_load_pd(S->density_field          + d_feqIndex);
+                    __m256d d_PD_pd = _mm256_load_pd(S->particle_distributions + d_index);
+
+
+                    // COMPUTATION DOTPROUCT & NORM
+
+                    __m256d dotX     = _mm256_mul_pd(   velX, dirX);
+                    __m256d dotXY    = _mm256_fmadd_pd( velY, dirY, dotX);
+                    __m256d dv_dot    = _mm256_fmadd_pd(velZ, dirZ, dotXY);
+                    __m256d normX     = _mm256_mul_pd(  velX, velX);
+                    __m256d normXY    = _mm256_fmadd_pd(velY, velY, normX);
+                    __m256d v_norm    = _mm256_fmadd_pd(velZ, velZ, normXY);
+                    __m256d b_dotX     = _mm256_mul_pd(   b_velX, dirX);
+
+                    __m256d b_dotXY    = _mm256_fmadd_pd( b_velY, dirY,   b_dotX);
+                    __m256d b_dv_dot    = _mm256_fmadd_pd(b_velZ, dirZ,   b_dotXY);
+                    __m256d b_normX     = _mm256_mul_pd(  b_velX, b_velX);
+                    __m256d b_normXY    = _mm256_fmadd_pd(b_velY, b_velY, b_normX);
+                    __m256d b_v_norm    = _mm256_fmadd_pd(b_velZ, b_velZ, b_normXY);
+
+                    __m256d c_dotX     = _mm256_mul_pd(   c_velX, dirX);
+                    __m256d c_dotXY    = _mm256_fmadd_pd( c_velY, dirY,   c_dotX);
+                    __m256d c_dv_dot    = _mm256_fmadd_pd(c_velZ, dirZ,   c_dotXY);
+                    __m256d c_normX     = _mm256_mul_pd(  c_velX, c_velX);
+                    __m256d c_normXY    = _mm256_fmadd_pd(c_velY, c_velY, c_normX);
+                    __m256d c_v_norm    = _mm256_fmadd_pd(c_velZ, c_velZ, c_normXY);
+
+                    __m256d d_dotX     = _mm256_mul_pd(   d_velX, dirX);
+                    __m256d d_dotXY    = _mm256_fmadd_pd( d_velY, dirY,   d_dotX);
+                    __m256d d_dv_dot    = _mm256_fmadd_pd(d_velZ, dirZ,   d_dotXY);
+                    __m256d d_normX     = _mm256_mul_pd(  d_velX, d_velX);
+                    __m256d d_normXY    = _mm256_fmadd_pd(d_velY, d_velY, d_normX);
+                    __m256d d_v_norm    = _mm256_fmadd_pd(d_velZ, d_velZ, d_normXY);
+
+
+                    // //////////////////////////////
+                    // COMPUTATION RETURN
+                    // //////////////////////////////
+
+                    __m256d tmpB          = _mm256_mul_pd(    df_pd,       tmpA);
+                    __m256d tmpC          = _mm256_mul_pd(    omTAUinv_pd, PD_pd);
+                    __m256d dotSqr        = _mm256_mul_pd(dv_dot, dv_dot);
+                    __m256d A             = _mm256_fmsub_pd( dotSqr,      CS2inv_pd, v_norm);
+                    __m256d B             = _mm256_fmadd_pd(dv_dot, CS2inv_pd, ones_pd);
+                    __m256d C             = _mm256_fmadd_pd(A, twoCS2inv_pd, B);
+                    __m256d D             = _mm256_fmadd_pd( tmpB,        C,            tmpC);
+
+
+                    __m256d b_tmpB          = _mm256_mul_pd(    b_df_pd,       tmpA);
+                    __m256d b_tmpC          = _mm256_mul_pd(    omTAUinv_pd,   b_PD_pd);
+                    __m256d b_dotSqr        = _mm256_mul_pd(b_dv_dot, b_dv_dot);
+                    __m256d b_A             = _mm256_fmsub_pd( b_dotSqr,      CS2inv_pd, b_v_norm);
+                    __m256d b_B             = _mm256_fmadd_pd(b_dv_dot, CS2inv_pd, ones_pd);
+                    __m256d b_C             = _mm256_fmadd_pd(b_A, twoCS2inv_pd, b_B);
+                    __m256d b_D             = _mm256_fmadd_pd( b_tmpB,        b_C,            b_tmpC);
+
+
+                    __m256d c_tmpB          = _mm256_mul_pd(    c_df_pd,       tmpA);
+                    __m256d c_tmpC          = _mm256_mul_pd(    omTAUinv_pd,   c_PD_pd);
+                    __m256d c_dotSqr        = _mm256_mul_pd(c_dv_dot, c_dv_dot);
+                    __m256d c_A             = _mm256_fmsub_pd( c_dotSqr,      CS2inv_pd, c_v_norm);
+                    __m256d c_B             = _mm256_fmadd_pd(c_dv_dot, CS2inv_pd, ones_pd);
+                    __m256d c_C             = _mm256_fmadd_pd(c_A, twoCS2inv_pd, c_B);
+                    __m256d c_D             = _mm256_fmadd_pd( c_tmpB,        c_C,            c_tmpC);
+
+
+                    __m256d d_tmpB          = _mm256_mul_pd(    d_df_pd,       tmpA);
+                    __m256d d_tmpC          = _mm256_mul_pd(    omTAUinv_pd,   d_PD_pd);
+                    __m256d d_dotSqr        = _mm256_mul_pd(d_dv_dot, d_dv_dot);
+                    __m256d d_A             = _mm256_fmsub_pd( d_dotSqr,      CS2inv_pd, d_v_norm);
+                    __m256d d_B             = _mm256_fmadd_pd(d_dv_dot, CS2inv_pd, ones_pd);
+                    __m256d d_C             = _mm256_fmadd_pd(d_A, twoCS2inv_pd, d_B);
+                    __m256d d_D             = _mm256_fmadd_pd( d_tmpB,        d_C,            d_tmpC);
+
+                    //////////////////
+                    // STORE
+                    //////////////////
+                    
+                    
+                    _mm256_storeu_pd(S->previous_particle_distributions+index,       D);
+                    _mm256_storeu_pd(S->previous_particle_distributions+b_index,   b_D);
+                    _mm256_storeu_pd(S->previous_particle_distributions+c_index,   c_D);
+                    _mm256_storeu_pd(S->previous_particle_distributions+d_index,   d_D);
+            }
+        }
+    }
+}
 
 
 // BLOCKING TEST ...
